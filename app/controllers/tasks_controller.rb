1822431a26cecc9b822a2aa24ff080daecb3eb8c
szyar <soezayarszyar@gmail.com>
2020-09-11 20:27:51 +0630

    implemented create and update functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,32 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:name, :detail))
+    if @task.save
+      flash[:notice] = "Task Created"
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(params.require(:task).permit(:name, :detail))
+      flash[:notice] = "Edited Successfully"
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
 end
