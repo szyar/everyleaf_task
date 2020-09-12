0e9a9c506b9e568b3fe59ae608d666aeb87aaede
szyar <soezayarszyar@gmail.com>
2020-09-12 14:25:50 +0630

    test model and structure the docs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
-
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Task model function', type: :model do
+  describe 'Validation test' do
+    context 'If the task title is empty' do
+      it 'It\'s hard to Validation' do
+        task = Task.new(name: '', detail: 'Failure test')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'If the task details are empty' do
+      it 'Validation is caught' do
+        task = Task.new(name: 'Failure test', detail: '')
+        expect(task).not_to be_valid
+      end
+    end
+    context 'When the content is described in the task title and details' do
+      it 'Validation passes' do
+        task = Task.new(name: 'Pass', detail: 'Pass test')
+        expect(task).to be_valid
+      end
+    end
+  end
 end
