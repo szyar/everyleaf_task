8734c5d409d1eab09181c819e745d7896f405e18
szyar <soezayarszyar@gmail.com>
2020-09-16 13:16:20 +0630

    finish spec writing

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,44 +1,42 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  FactoryBot.create(:first_task)
-  FactoryBot.create(:second_task)
 
-  # context "Create new task" do
-  #   before(:each) do
-  #     visit new_task_path
-  #     within("form") do
-  #       fill_in "Name", with: "Test Name"
-  #       fill_in "Detail", with: "Test detail"
-  #     end
-  #   end
-  #   scenario "New task is registered" do
-  #     click_button "Create Task"
-  #     expect(page).to have_content("Task Created")
-  #   end
-  #   scenario "The registered task is displayed" do
-  #     click_button "Create Task"
-  #     expect(page).to have_content("Test Name")
-  #   end
-  # end
+  context "Create new task" do
+    before(:each) do
+      visit new_task_path
+      within("form") do
+        fill_in "Name", with: "Test Name"
+        fill_in "Detail", with: "Test detail"
+      end
+    end
+    scenario "New task is registered" do
+      click_button "Create Task"
+      expect(page).to have_content("Task Created")
+    end
+    scenario "The registered task is displayed" do
+      click_button "Create Task"
+      expect(page).to have_content("Test Name")
+    end
+  end
 
-  # context "Check Task Lists" do
-  #   before(:each) do
-  #     visit tasks_path
-  #   end
-  #   scenario "Task list page is displayed" do
-  #     expect(page).to have_content("Task List")
-  #   end
-  # end
+  context "Check Task Lists" do
+    before(:each) do
+      visit tasks_path
+    end
+    scenario "Task list page is displayed" do
+      expect(page).to have_content("Task List")
+    end
+  end
 
   context 'When tasks are arranged in descending order of creation date and time' do
-    context 'When tasks are arranged in descending order of creation date and time' do
-      it 'New task is displayed at the top' do
-        # Implement here
-        tasks = Task.all.order(created_at: :desc)
-        task = Task.last
-        expect(task.id).to eq(101)
-      end
+    it 'New task is displayed at the top' do
+        FactoryBot.create(:first_task)
+        FactoryBot.create(:second_task)
+        visit tasks_path
+        task_list = Task.all.order(created_at: :desc)
+        task = task_list.first
+        expect(task.name).to eq("Name 2")
     end
   end
 
