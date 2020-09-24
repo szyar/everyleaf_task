3f344b4bd3a17f98647a77806f06ce2cdc9090b9
szyar <soezayarszyar@gmail.com>
2020-09-24 21:02:26 +0630

    add search indexes and log results after adding indexes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_141030) do
+ActiveRecord::Schema.define(version: 2020_09_24_142004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_09_23_141030) do
     t.datetime "expired_at"
     t.string "status"
     t.integer "priority_type"
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
   create_table "users", force: :cascade do |t|
