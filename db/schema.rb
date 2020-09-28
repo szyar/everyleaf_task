82d7c266504c80299d9476fced4977378baa34c5
szyar <soezayarszyar@gmail.com>
2020-09-28 13:42:41 +0630

    add email column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_142004) do
+ActiveRecord::Schema.define(version: 2020_09_28_071143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_09_24_142004) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
