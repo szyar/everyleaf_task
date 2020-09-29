3997d4a42b36e7521819a18da60976b023b02de8
szyar <soezayarszyar@gmail.com>
2020-09-29 08:51:10 +0630

    create tasks with fill in and select instead of factory bot

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -10,19 +10,21 @@ RSpec.feature "Tasks", type: :feature do
       within("form") do
         click_button "Log In"
         visit new_task_path
+        fill_in "Name", with: "Test Name"
+        fill_in "Detail", with: "Test Name Detail"
+        select '2020', from: :task_expired_at_1i
+        select '9月', from: :task_expired_at_2i
+        select '29', from: :task_expired_at_3i
+        select 'Unstarted', from: :task_status
+        select 'High', from: :task_priority_type
+        click_button "Create Task"
       end
     end
     it "New task is registered with deadline" do
-      FactoryBot.create(:first_task)
-      task = Task.first
-      visit task_path(task)
-      expect(page).to have_content("September 26, 2020 19:32")
+      expect(page).to have_content("Task Created")
     end
     it "The registered task is displayed" do
-      FactoryBot.create(:first_task)
-      task = Task.first
-      visit task_path(task)
-      expect(page).to have_content("Name 1")
+      expect(page).to have_content("Test Name")
     end
   end
 
@@ -40,10 +42,33 @@ RSpec.feature "Tasks", type: :feature do
   end
 
   context 'When tasks are arranged in descending order of creation date and time' do
+    before(:each) do
+      visit root_path
+      fill_in "Username", with: "szyar"
+      fill_in "Password", with: "batman"
+      within("form") do
+        click_button "Log In"
+        visit new_task_path
+        fill_in "Name", with: "Name 1"
+        fill_in "Detail", with: "Name 1 Detail"
+        select '2020', from: :task_expired_at_1i
+        select '9月', from: :task_expired_at_2i
+        select '29', from: :task_expired_at_3i
+        select 'Unstarted', from: :task_status
+        select 'High', from: :task_priority_type
+        click_button "Create Task"
+      end
+    end
     it 'New task is displayed at the top' do
-        FactoryBot.create(:first_task)
-        FactoryBot.create(:second_task)
-        visit tasks_path
+        visit new_task_path
+        fill_in "Name", with: "Name 2"
+        fill_in "Detail", with: "Name 2 Detail"
+        select '2020', from: :task_expired_at_1i
+        select '9月', from: :task_expired_at_2i
+        select '29', from: :task_expired_at_3i
+        select 'Unstarted', from: :task_status
+        select 'High', from: :task_priority_type
+        click_button "Create Task"
         task_list = Task.all.order(created_at: :desc)
         task = task_list.first
         expect(task.name).to eq("Name 2")
@@ -51,10 +76,33 @@ RSpec.feature "Tasks", type: :feature do
   end
 
   context 'Check sorting by deadline' do
+    before(:each) do
+      visit root_path
+      fill_in "Username", with: "szyar"
+      fill_in "Password", with: "batman"
+      within("form") do
+        click_button "Log In"
+        visit new_task_path
+        fill_in "Name", with: "Name 1"
+        fill_in "Detail", with: "Name 1 Detail"
+        select '2020', from: :task_expired_at_1i
+        select '9月', from: :task_expired_at_2i
+        select '29', from: :task_expired_at_3i
+        select 'Unstarted', from: :task_status
+        select 'High', from: :task_priority_type
+        click_button "Create Task"
+      end
+    end
     it 'Task with later date is displayed at the top' do
-        FactoryBot.create(:first_task)
-        FactoryBot.create(:second_task)
-        visit tasks_path
+        visit new_task_path
+        fill_in "Name", with: "Name 2"
+        fill_in "Detail", with: "Name 2 Detail"
+        select '2020', from: :task_expired_at_1i
+        select '9月', from: :task_expired_at_2i
+        select '30', from: :task_expired_at_3i
+        select 'Unstarted', from: :task_status
+        select 'High', from: :task_priority_type
+        click_button "Create Task"
         task_list = Task.all.order(expired_at: :desc)
         task = task_list.first
         expect(task.name).to eq("Name 2")
@@ -62,13 +110,36 @@ RSpec.feature "Tasks", type: :feature do
   end
 
   context 'When registering a new task, can also register the status' do
+    before(:each) do
+      visit root_path
+      fill_in "Username", with: "szyar"
+      fill_in "Password", with: "batman"
+      within("form") do
+        click_button "Log In"
+        visit new_task_path
+        fill_in "Name", with: "Name 1"
+        fill_in "Detail", with: "Name 1 Detail"
+        select '2020', from: :task_expired_at_1i
+        select '9月', from: :task_expired_at_2i
+        select '29', from: :task_expired_at_3i
+        select 'Unstarted', from: :task_status
+        select 'High', from: :task_priority_type
+        click_button "Create Task"
+      end
+    end
     it 'Status is displayed' do
-        FactoryBot.create(:first_task)
-        FactoryBot.create(:second_task)
-        visit tasks_path
-        task_list = Task.all
-        task = task_list.first
-        expect(task.status).to eq("Unstarted")
+      visit new_task_path
+      fill_in "Name", with: "Name 2"
+      fill_in "Detail", with: "Name 2 Detail"
+      select '2020', from: :task_expired_at_1i
+      select '9月', from: :task_expired_at_2i
+      select '29', from: :task_expired_at_3i
+      select 'Completed', from: :task_status
+      select 'High', from: :task_priority_type
+      click_button "Create Task"
+      task_list = Task.all
+      task = task_list.first
+      expect(task.status).to eq("Unstarted")
     end
   end
 
@@ -106,13 +177,36 @@ RSpec.feature "Tasks", type: :feature do
   end
 
   context 'When tasks are arranged in descending order of priority' do
+    before(:each) do
+      visit root_path
+      fill_in "Username", with: "szyar"
+      fill_in "Password", with: "batman"
+      within("form") do
+        click_button "Log In"
+        visit new_task_path
+        fill_in "Name", with: "Name 1"
+        fill_in "Detail", with: "Name 1 Detail"
+        select '2020', from: :task_expired_at_1i
+        select '9月', from: :task_expired_at_2i
+        select '29', from: :task_expired_at_3i
+        select 'Unstarted', from: :task_status
+        select 'Low', from: :task_priority_type
+        click_button "Create Task"
+      end
+    end
     it 'Task with higher priority is displayed at the top' do
-        FactoryBot.create(:first_task)
-        FactoryBot.create(:second_task)
-        visit tasks_path
-        task_list = Task.all.order(priority_type: :desc)
-        task = task_list.first
-        expect(task.name).to eq("Name 2")
+      visit new_task_path
+      fill_in "Name", with: "Name 2"
+      fill_in "Detail", with: "Name 2 Detail"
+      select '2020', from: :task_expired_at_1i
+      select '9月', from: :task_expired_at_2i
+      select '29', from: :task_expired_at_3i
+      select 'Unstarted', from: :task_status
+      select 'High', from: :task_priority_type
+      click_button "Create Task"
+      task_list = Task.all.order(priority_type: :desc)
+      task = task_list.first
+      expect(task.name).to eq("Name 2")
     end
   end
 
