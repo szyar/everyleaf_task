ff869ffb87f7edbdc2e956528e44c2b7faea6d72
szyar <soezayarszyar@gmail.com>
2020-09-29 16:18:58 +0630

    add admin column to users table to create admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_090556) do
+ActiveRecord::Schema.define(version: 2020_09_29_094527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_090556) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "tasks", "users"
