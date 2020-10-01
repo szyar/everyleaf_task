0ec2169605ec4bd60721f7224959de2099bef2e3
szyar <soezayarszyar@gmail.com>
2020-10-01 14:26:56 +0630

    test log out functioinality

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -2,32 +2,32 @@ require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
 
-  # describe "User registration test" do
-  #   context "Register new user" do
-  #     before(:each) do
-  #       visit root_path
-  #       click_link "Normal user signup"
-  #       within("form") do
-  #         fill_in('Username', with: 'John')
-  #         fill_in('Email', with: 'john@test.com')
-  #         fill_in('Password', with: 'hellojohn')
-  #         fill_in('Password confirmation', with: 'hellojohn')
-  #       end
-  #     end
-  #     it "New user is registered" do
-  #       click_button "Create User"
-  #       expect(page).to have_content("Welcome John, you have successfully signed up")
-  #     end
-  #   end
-  #   context "Check task list without login" do
-  #     before(:each) do
-  #       visit tasks_path
-  #     end
-  #     it "Redirect to login with message" do
-  #       expect(page).to have_content("You must be logged in to perform that action")
-  #     end
-  #   end
-  # end
+  describe "User registration test" do
+    context "Register new user" do
+      before(:each) do
+        visit root_path
+        click_link "Normal user signup"
+        within("form") do
+          fill_in('Username', with: 'John')
+          fill_in('Email', with: 'john@test.com')
+          fill_in('Password', with: 'hellojohn')
+          fill_in('Password confirmation', with: 'hellojohn')
+        end
+      end
+      it "New user is registered" do
+        click_button "Create User"
+        expect(page).to have_content("Welcome John, you have successfully signed up")
+      end
+    end
+    context "Check task list without login" do
+      before(:each) do
+        visit tasks_path
+      end
+      it "Redirect to login with message" do
+        expect(page).to have_content("You must be logged in to perform that action")
+      end
+    end
+  end
 
   describe "Testing session functionality" do
     context "Be able to login" do
@@ -72,6 +72,20 @@ RSpec.feature "Users", type: :feature do
         expect(page).to have_content("Task List")
       end
     end
+    context "Test log out functionality" do
+      before(:each) do
+        visit root_path
+        within("form") do
+          fill_in('Username', with: 'szyar')
+          fill_in('Password', with: 'batman')
+          click_button "Log In"
+        end
+      end
+      it "Log out test pass" do
+        click_link "LogOut"
+        expect(page).to have_content("Logged out")
+      end
+    end
   end
-
+  
 end
