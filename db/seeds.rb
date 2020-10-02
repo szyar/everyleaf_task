9af342c9c63126fa59f89a22a87c392004710c2c
szyar <soezayarszyar@gmail.com>
2020-10-02 20:10:06 +0630

    prepare task seed datas for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,35 @@
 # User.create(username: 'test4', email:'test4@test.com', password:'hellotest4')
 # User.create(username: 'test5', email:'test5@test.com', password:'hellotest5')
 
-User.create(username: 'seedadmin', email:'seed@admin.com', password:'helloseedadmin', admin: true)
+# User.create(username: 'seedadmin', email:'seed@admin.com', password:'helloseedadmin', admin: true)
+
+Task.create(name: "Name 1", detail: "Detail 1",
+  expired_at: '2020-10-5 19:32:00', status: "Unstarted", priority_type: "High",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 2", detail: "Detail 2",
+  expired_at: '2020-10-3 19:32:00', status: "Completed", priority_type: "Medium",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 3", detail: "Detail 3",
+  expired_at: '2020-10-4 19:32:00', status: "In Progress", priority_type: "Low",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 4", detail: "Detail 4",
+  expired_at: '2020-10-6 19:32:00', status: "Completed", priority_type: "Medium",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 5", detail: "Detail 5",
+  expired_at: '2020-10-5 19:32:00', status: "In Progress", priority_type: "Low",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 6", detail: "Detail 6",
+  expired_at: '2020-10-7 19:32:00', status: "Unstarted", priority_type: "Low",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 7", detail: "Detail 7",
+  expired_at: '2020-10-4 19:32:00', status: "In Progress", priority_type: "Low",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 8", detail: "Detail 8",
+  expired_at: '2020-10-3 19:32:00', status: "Unstarted", priority_type: "Medium",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 9", detail: "Detail 9",
+  expired_at: '2020-10-3 19:32:00', status: "Unstarted", priority_type: "High",
+  user_id: User.first.id, label_ids: Label.first.id)
+Task.create(name: "Name 10", detail: "Detail 10",
+  expired_at: '2020-10-9 19:32:00', status: "In Progress", priority_type: "High",
+  user_id: User.first.id, label_ids: Label.first.id)
