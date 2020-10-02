6fe68570fb7fedffc7cb0b3b08a17ce453a24ea0
szyar <soezayarszyar@gmail.com>
2020-10-02 15:15:58 +0630

    start association on tasks and labels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_081148) do
+ActiveRecord::Schema.define(version: 2020_10_02_084346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2020_10_02_081148) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "task_labels", force: :cascade do |t|
+    t.integer "task_id"
+    t.integer "label_id"
   end
 
   create_table "tasks", force: :cascade do |t|
