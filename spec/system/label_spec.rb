d6135e7312381fddc58d50c0f89f20e8f085c732
szyar <soezayarszyar@gmail.com>
2020-10-02 19:57:07 +0630

    add more label tests

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -30,8 +30,38 @@ RSpec.feature "Labels", type: :feature do
     end
     scenario "Label list page is displayed" do
       click_button "Log In"
-      visit labels_path
+      click_link "Labels"
       expect(page).to have_content("Labels List")
+    end
+  end
+
+  context "Label details page test" do
+    before(:each) do
+      visit root_path
+      fill_in "Username", with: "szyar"
+      fill_in "Password", with: "batman"
+    end
+    it "Label details page displayed" do
+      click_button "Log In"
+      click_link "Labels"
+      click_link "Show", match: :first
+      expect(page).to have_content("Showing Label Details")
+    end
+  end
+
+  context "Edit label test" do
+    before(:each) do
+      visit root_path
+      fill_in "Username", with: "szyar"
+      fill_in "Password", with: "batman"
+    end
+    it "Label edited successfully" do
+      click_button "Log In"
+      click_link "Labels"
+      click_link "Edit", match: :first
+      fill_in "Name", with: "Edited Name"
+      click_button "Update Label"
+      expect(page).to have_content("Edited Successfully")
     end
   end
 
@@ -47,11 +77,6 @@ RSpec.feature "Labels", type: :feature do
         visit labels_path
       end
     end
-    it "Search by id passes" do
-      label = Label.first
-      result = Label.find(label.id)
-      expect(result.name).to eq('Label 1')
-    end
     it "Show associated tasks by searching label" do
       FactoryBot.create(:first_task)
       FactoryBot.create(:second_task)
@@ -60,6 +85,20 @@ RSpec.feature "Labels", type: :feature do
       tasks = result.tasks
       task = tasks.first
       expect(task.name).to eq('Name 1')
+    end
+  end
+
+  context "Delete label test" do
+    before(:each) do
+      visit root_path
+      fill_in "Username", with: "szyar"
+      fill_in "Password", with: "batman"
+    end
+    it "Label deleted successfully" do
+      click_button "Log In"
+      click_link "Labels"
+      click_link "Delete", match: :first
+      expect(page).to have_content("Deleted Successfully")
     end
   end
 end
